bdd6140a9a29bd66c5cecfccd40d18bbf036ab5e
LikhithKorivi <145471254+LikhithKorivi@users.noreply.github.com>
2025-01-27 15:15:32 +0530

    Add files via upload

---
+++ src/main/java/com/interceptica/riskAscend/subfinder/SubfinderApplication.java
@@ -0,0 +1,13 @@
+package com.interceptica.riskAscend.subfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interceptica/riskAscend/subfinder/SubfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interceptica.riskAscend.subfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
